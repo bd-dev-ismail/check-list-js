fc9a75041060faff4295fc4b59d58ec70cd017e0
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-12 18:44:16 +0600

    var let & const

---
+++ let-const.js
@@ -0,0 +1,16 @@
+let a = 30;
+a = 20;
+// console.log(a);
+const b = 50;
+// b= 40;
+console.log(b);
+/**
+ * Var
+ * var1- Value can be overwrite or reassign possiable
+ * var2- Same variabel re declare possible.
+ * let
+ * let1-Also same as var like value reassign possible.
+ * let2-No possible to re delare same variabel. 
+ * const - constant vaiabel
+ * const-Can't overwitre or reassign any variabel & can't re declare it.
+ */
