49002d3cd9b97640e0c30cfe1fe00bb2143aeab2
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-13 15:21:48 +0600

    true falsy

---
+++ truefalse.js
@@ -0,0 +1,20 @@
+/**
+ * ৩. true false কখন ইউজ  করে। সেটা জানতে হবে। কি কি ধরনের জিনিস জাভাস্ক্রিপ্ট এর truthy আর কি কি জিনিস জাভাস্ক্রিপ্ট এ falsy সেটা জানতে হবে।
+ */
+// console.log(Boolean(10 < 12));
+let x = 5;
+// == equal to 
+// console.log(x == 5)
+// console.log(x == 8);
+// console.log(x == '5');
+let y = 10;
+// equal value and equal type
+// console.log(y === 10);
+// console.log(y === '10')
+//!=	not equal
+// console.log(x != 8);
+// console.log(y !== 10)
+// console.log(x < 8);
+// console.log(y > 8);
+// console.log(x >= 8);
+// console.log(x <= 8);
